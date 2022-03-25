df7dbcdb6ebb3482674318f1adbae952880bb77a
xxidbr9 <barnando13@gmail.com>
2022-03-25 10:34:53 +0700

    chore: revisi dan informasi

--- src/components/AboutAuthor.jsx
+++ src/components/AboutAuthor.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import NavAbout from './NavAbout/NavAbout';
+import React from "react";
+import NavAbout from "./NavAbout/NavAbout";
+import Navbar from "./Navbar";
 const AboutAuthor = () => {
-	return (
-		<>
-			<NavAbout />
-			<div
-				style={{
-					textAlign: 'left',
-					width: '50%',
-					margin: 'auto',
-				}}
-			>
-				<h1>About the Author</h1>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
-					delectus velit ducimus, molestias maxime vero quis voluptates. Sunt,
-					autem iste!
-				</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar isAboutPage={true} />
+      <div
+        style={{
+          textAlign: "left",
+          width: "50%",
+          margin: "auto"
+        }}>
+        <h1>About the Author</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          delectus velit ducimus, molestias maxime vero quis voluptates. Sunt,
+          autem iste!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default AboutAuthor;
--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -11,3 +11,12 @@ export const SidebarData = [
     cName: "sidebar-item",
   },
 ];
+
+// kalau bukan components export pakai camel case, untuk membedakan yang di import itu sebuah components atau cuma return value
+export const additionalData = [
+  {
+    title: 'About Author',
+    path: "/about/about-author",
+    cName: "sidebar-item",
+  }
+]
